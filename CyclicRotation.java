3d71fd85afb717f75603f57f2cb3c15151ebd65f
zenmony <32320605+zenmony@users.noreply.github.com>
2017-09-28 16:14:10 +0900

    Create CyclicRotation.java

---
+++ CyclicRotation.java
@@ -0,0 +1,41 @@
+/**
+* A which is shifted to the right by K indexes
+* @author zenmony
+*
+*/
+class CyclicRotation {
+/**
+ * return A which is shifted to the right by K indexes
+ * @param A zero-indexed array A consisting of N integers
+ * @param K  A will be shifted to the right by K indexes.
+ * @return B which is shifted to the right by K indexes
+ */
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        
+        // validation check
+		if (A == null || A.length ==0 )
+			return A;
+
+		if ( A.length == 1) {
+			return A;
+		}
+		
+		// N is the lenght of Array A
+		int N = A.length;
+
+		// rotate number
+		int rotate = K % N;
+		
+		// new Array
+		int[] B = new int[N];
+		
+		// shift
+		for (int i = 0; i < N; i++) {
+			B[(i + rotate) % N] = A[i];
+		}
+		
+		return B;
+
+    }
+}
