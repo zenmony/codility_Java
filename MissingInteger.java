d8e10036dbf18c4172c1135c826c4697b0154dd5
zenmony <32320605+zenmony@users.noreply.github.com>
2017-09-29 09:33:51 +0900

    Update MissingInteger.java

--- MissingInteger.java
+++ MissingInteger.java
@@ -3,34 +3,39 @@
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
-import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
-class Solution {
+class MissingInteger {
     public int solution(int[] A) {
         // write your code in Java SE 8
-                        // validation check
-		if (A == null || A.length ==0 )
+                        
+	// validation check
+	if (A == null || A.length ==0 )
 			return 0;
-
-		// sort for easy check
-		Arrays.sort(A);
-		
-		// All negative elements
-		if (A[A.length-1] < 1)
-			return 1;
-
-		// judge
-		for (int i = 0; i<A.length-1; i++) {
-			if (A[i] < 1)
-				continue;
-			else if (A[i] == A[i+1] || A[i]+1 == A[i+1])
-				continue;
-			else
-				return A[i]+1;
-		}
-		
-		// max 
-		return A.length + 1;
-
+	
+	// TreeSet for easy checking
+    Set<Integer> set = new TreeSet<>();
+    
+    // auto-sort and positive value only
+    for (int i=0; i<A.length; i++) {
+    	// positive value only
+		if (A[i] > 0) {
+			// auto-sort by TreeSet
+    		set.add(A[i]);
+ 		}
+ 	}
+ 	
+ 	
+ 	// search missing integer
+ 	int cnt = 1;
+	for(Integer value : set){
+		if (cnt != value)
+			return cnt;
+		cnt++;
+	}
+	
+	// max 
+	return cnt;
     }
 }
