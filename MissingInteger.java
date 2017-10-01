164f76b70763fd36d9ea71946d2ffae5b0108501
zenmony <32320605+zenmony@users.noreply.github.com>
2017-10-01 13:01:08 +0900

    Update MissingInteger.java

--- MissingInteger.java
+++ MissingInteger.java
@@ -1,41 +1,45 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 import java.util.Set;
 import java.util.TreeSet;
-
+/**
+* Missing Integer
+*
+* @author zenmony
+* @version 1.0
+*
+*/
 class MissingInteger {
+	/**
+	 * Missing Integer
+	 * @param  zero-indexed array H of N positive integers
+	 * @return the smallest positive integer (greater than 0) that does not occur in A.
+	 */
     public int solution(int[] A) {
-        // write your code in Java SE 8
-                        
-	// validation check
-	if (A == null || A.length ==0 )
-			return 0;
-	
-	// TreeSet for easy checking
-    Set<Integer> set = new TreeSet<>();
-    
-    // auto-sort and positive value only
-    for (int i=0; i<A.length; i++) {
-    	// positive value only
-		if (A[i] > 0) {
-			// auto-sort by TreeSet
-    		set.add(A[i]);
- 		}
- 	}
- 	
- 	
- 	// search missing integer
- 	int cnt = 1;
-	for(Integer value : set){
-		if (cnt != value)
-			return cnt;
-		cnt++;
-	}
-	
-	// max 
-	return cnt;
+		// validation check
+		if (A == null || A.length ==0 )
+				return 0;
+
+		// TreeSet for easy checking
+	    Set<Integer> set = new TreeSet<>();
+
+	    // auto-sort and positive value only
+	    for (int i=0; i<A.length; i++) {
+	    	// positive value only
+			if (A[i] > 0) {
+				// auto-sort by TreeSet
+	    		set.add(A[i]);
+	 		}
+	 	}
+
+
+	 	// search missing integer
+	 	int cnt = 1;
+		for(Integer value : set){
+			if (cnt != value)
+				return cnt;
+			cnt++;
+		}
+
+		// max
+		return cnt;
     }
 }
