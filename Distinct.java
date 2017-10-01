ca8b2c5ddd63195d44de2c0fef6562b93269f5b2
zenmony <32320605+zenmony@users.noreply.github.com>
2017-10-01 13:04:23 +0900

    Update Distinct.java

--- Distinct.java
+++ Distinct.java
@@ -1,25 +1,30 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
-import java.util.Set;
 import java.util.HashSet;
-
+import java.util.Set;
+/**
+* Distinct
+*
+* @author zenmony
+* @version 1.0
+*
+*/
 class  Distinct {
+	/**
+	 * Distinct
+	 * @param  a zero-indexed array A consisting of N integers
+	 * @return the number of distinct values in array A
+	 */
     public int solution(int[] A) {
-        // write your code in Java SE 8
         // validation check
 		if (A == null || A.length ==0 )
 			return 0;
-	
+
 		// Set for easy checking
     	Set<Integer> set = new HashSet<>( );
      	for (int i = 0; i<A.length ;i++){
      		set.add(A[i]);
     	}
-        
+
         return set.size();
-    
+
     }
 }
